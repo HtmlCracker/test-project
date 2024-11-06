187508e1ca94e0a1e606a96b557cea63ce19303f
HtmlCracker <daniilprogrammer3.14@gmail.com>
2024-11-07 00:47:03 +0300

    Rename ProfileDto -> ProfileResponseDto

--- profile-service/src/main/java/org/example/api/factories/ProfileDtoFactory.java
+++ profile-service/src/main/java/org/example/api/factories/ProfileDtoFactory.java
@@ -1,13 +1,13 @@
 package org.example.api.factories;
 
-import org.example.api.dto.ProfileDto;
+import org.example.api.dto.response.ProfileResponseDto;
 import org.example.api.entities.ProfileEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProfileDtoFactory {
-    public ProfileDto makeProfileDto(ProfileEntity entity) {
-        return ProfileDto.builder()
+    public ProfileResponseDto makeProfileDto(ProfileEntity entity) {
+        return ProfileResponseDto.builder()
                 .id(entity.getId())
                 .email(entity.getEmail())
                 .name(entity.getName())
