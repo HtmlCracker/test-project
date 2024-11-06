bdf4bbc5229195795209a7c37e7263172f32f90e
HtmlCracker <daniilprogrammer3.14@gmail.com>
2024-11-06 23:49:39 +0300

    Add findByEmail to ProfileRepository

--- profile-service/src/main/java/org/example/api/repositories/ProfileRepository.java
+++ profile-service/src/main/java/org/example/api/repositories/ProfileRepository.java
@@ -1,9 +1,11 @@
-package org.example.store.repositories;
+package org.example.api.repositories;
 
-import org.example.store.entities.ProfileEntity;
+import org.example.api.entities.ProfileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProfileRepository extends JpaRepository<ProfileEntity, UUID> {
+    Optional<ProfileEntity> findByEmail(String email);
 }
