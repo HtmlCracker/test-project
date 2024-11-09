c6da4cbc40326ab4c6fa4d1b1f5aceb7fd456fe1
HtmlCracker <daniilprogrammer3.14@gmail.com>
2024-11-09 23:07:42 +0300

    Add ProfileUpdateRequestDto
    Add dto for update the profile

---
+++ profile-service/src/main/java/org/example/api/dto/request/ProfileUpdateRequestDto.java
@@ -0,0 +1,26 @@
+package org.example.api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProfileUpdateRequestDto {
+    @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", message = "Email should be valid")
+    String email;
+
+    @NotBlank(message = "Name can't be empty")
+    String name;
+
+    String surname;
+
+    String description;
+
+    String roles;
+}
+
