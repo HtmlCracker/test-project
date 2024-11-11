843080e18d12eebeb607dc1ada6657a0e1265580
HtmlCracker <daniilprogrammer3.14@gmail.com>
2024-11-11 20:57:18 +0300

    Add @NotNull to email field

--- profile-service/src/main/java/org/example/api/dto/request/ProfileRegistrationRequestDto.java
+++ profile-service/src/main/java/org/example/api/dto/request/ProfileRegistrationRequestDto.java
@@ -2,6 +2,7 @@ package org.example.api.dto.request;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -13,6 +14,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProfileRegistrationRequestDto {
     @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", message = "Email should be valid")
+    @NotNull
     String email;
 
     @NotBlank(message = "Name can't be empty")
