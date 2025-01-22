231ae14e2e8e2f65bdfa4a7d0aa8fbe13d714714
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-01-22 15:08:59 +0300

    Create test class

---
+++ file-storage-service/src/test/java/com/example/controller/StorageControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StorageControllerTest {
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:alpine")
+    );
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.generate-ddl", () -> true);
+    }
+
+
+
+}
