4e4f1d777c5efd1666564ec9777d92eb65d91f67
HtmlCracker <131031623+HtmlCracker@users.noreply.github.com>
2025-03-29 21:37:51 +0300

    На время разработки настроил полную CORS-разблокировку

--- security-service/src/main/java/com/example/securityservice/config/SecurityConfig.java
+++ security-service/src/main/java/com/example/securityservice/config/SecurityConfig.java
@@ -47,10 +47,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:45292"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
