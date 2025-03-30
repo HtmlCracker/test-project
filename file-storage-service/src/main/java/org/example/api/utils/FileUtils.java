a9aed49c0dee226caf9627a371c011bd41eadfff
HtmlCracker <131031623+HtmlCracker@users.noreply.github.com>
2025-03-30 15:11:46 +0300

    feat: add mehod mkDir
    - Добавил метод для создания директории

--- file-storage-service/src/main/java/org/example/api/utils/FileUtils.java
+++ file-storage-service/src/main/java/org/example/api/utils/FileUtils.java
@@ -2,6 +2,14 @@ package org.example.api.utils;
 
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+
 @Component
 public class FileUtils {
+    public void mkDir(String pathName) {
+        File theDir = new File(pathName);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+    }
 }
