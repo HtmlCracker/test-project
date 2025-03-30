c21ad506c76c773634e3a7b11190f9eb6eefdadf
HtmlCracker <131031623+HtmlCracker@users.noreply.github.com>
2025-03-30 15:13:17 +0300

    feat: add automatic directories creation on startup
    При запуске приложеня создаются директории:
     - files/temporary-storage
     - files/compressed-storage
     - files/encrypted-storage
     - files/permanent-storage

---
+++ file-storage-service/src/main/java/org/example/api/configs/RunAfterStartup.java
@@ -0,0 +1,37 @@
+package org.example.api.configs;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.example.api.utils.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Component
+public class RunAfterStartup {
+    @Autowired
+    FileUtils fileUtils;
+
+    @Value("${PATH_TO_TEMPORARY_STORAGE}")
+    private String temporaryStoragePath;
+
+    @Value("${PATH_TO_COMPRESSED_STORAGE}")
+    private String compressedStoragePath;
+
+    @Value("${PATH_TO_ENCRYPTED_STORAGE}")
+    private String encryptedStoragePath;
+
+    @Value("${PATH_TO_PERMANENT_STORAGE}")
+    private String permanentStoragePath;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        fileUtils.mkDir(temporaryStoragePath);
+        fileUtils.mkDir(compressedStoragePath);
+        fileUtils.mkDir(encryptedStoragePath);
+        fileUtils.mkDir(permanentStoragePath);
+    }
+}
