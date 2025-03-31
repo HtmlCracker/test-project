52d63c3a9c2560860540d13ac5da521450366386
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-03-31 23:29:24 +0300

    feat: [draft] add temporary file upload logic
     - сохраняются файлы с именами на основе хэша
     - сохраняем информацию о файле в бд

--- file-storage-service/src/main/java/org/example/api/services/StorageService.java
+++ file-storage-service/src/main/java/org/example/api/services/StorageService.java
@@ -1,7 +1,49 @@
 package org.example.api.services;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.example.api.entities.FileInfoEntity;
+import org.example.api.enums.FileStates;
+import org.example.api.repositories.FileInfoRepository;
+import org.example.api.utils.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class StorageService {
+    final FileInfoRepository fileInfoRepository;
+    final FileUtils fileUtils;
+
+    @Value("${PATH_TO_TEMPORARY_STORAGE}")
+    private String temporaryStoragePath;
+
+    public FileInfoEntity temporaryUploadFile(MultipartFile file) {
+        String fileHash = fileUtils.calculateFileHash(file);
+        String filePath = fileUtils.createFileInDir(fileHash, file, temporaryStoragePath);
+        FileInfoEntity fileInfoEntity = constructNewEntity(file, fileHash, filePath);
+        FileInfoEntity savedFileInfoEntity = saveFileInfoEntity(fileInfoEntity);
+
+        return savedFileInfoEntity;
+    }
+
+    private FileInfoEntity saveFileInfoEntity(FileInfoEntity fileInfoEntity) {
+        return fileInfoRepository.save(fileInfoEntity);
+    }
+
+    private FileInfoEntity constructNewEntity(MultipartFile file,
+                                              String fileHash,
+                                              String filePath) {
+        String origFileName = file.getOriginalFilename();
+        return FileInfoEntity.builder()
+                .originalFileName(origFileName)
+                .originalFileSize(file.getSize())
+                .fileState(FileStates.UPLOADED)
+                .fileHash(fileHash)
+                .filePath(filePath)
+                .build();
+    }
 }
