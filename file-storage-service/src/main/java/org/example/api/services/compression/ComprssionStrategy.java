f307f63ae09460be56f9f98236a7fa0f8d50b7b3
HtmlCracker <131031623+HtmlCracker@users.noreply.github.com>
2025-03-31 17:08:36 +0300

    feat: add interface ComprssionStrategy
    Интерфейс описывает для различных стратегий компрессии методы:
     - compress
     - decompress

---
+++ file-storage-service/src/main/java/org/example/api/services/compression/ComprssionStrategy.java
@@ -0,0 +1,9 @@
+package org.example.api.services.compression;
+
+import java.io.InputStream;
+
+public interface ComprssionStrategy {
+    byte[] compress(InputStream inputStream);
+
+    byte[] deCompress(InputStream inputStream);
+}
