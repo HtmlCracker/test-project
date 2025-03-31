d982c6c2ce249501e9090ac178e60a04f264cd52
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-03-31 23:13:17 +0300

    refactor: optimize imports

--- file-storage-service/src/main/java/org/example/api/entities/FileInfoEntity.java
+++ file-storage-service/src/main/java/org/example/api/entities/FileInfoEntity.java
@@ -7,7 +7,6 @@ import org.example.api.enums.FileStates;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
