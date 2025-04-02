e4dfc01dfa334c45ccda5cf3ae1b7fe8b2183cb5
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-02 21:36:28 +0300

    feat: add findByFilePath to FileInfoRepository
    Добавлен метод для получения сущности по filePath:
     - Возвращает Optional<FileInfoEntity>

--- file-storage-service/src/main/java/org/example/api/repositories/FileInfoRepository.java
+++ file-storage-service/src/main/java/org/example/api/repositories/FileInfoRepository.java
@@ -3,8 +3,9 @@ package org.example.api.repositories;
 import org.example.api.entities.FileInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FileInfoRepository extends JpaRepository<FileInfoEntity, UUID> {
-
+    Optional<FileInfoEntity> findByFilePath(String filePath);
 }
