1306c211359162744dec3b21529875dae6660b01
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-02 21:36:52 +0300

    WIP: add for tests

--- file-storage-service/src/main/java/org/example/api/controllers/FileStorageController.java
+++ file-storage-service/src/main/java/org/example/api/controllers/FileStorageController.java
@@ -9,6 +9,7 @@ import org.example.api.entities.FileInfoEntity;
 import org.example.api.exceptions.BadRequestException;
 import org.example.api.factories.response.UploadFileResponseDtoFactory;
 import org.example.api.services.StorageService;
+import org.example.api.services.UploadProcessor;
 import org.example.api.utils.FileUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Transactional
@@ -24,13 +26,12 @@ import java.io.FileNotFoundException;
 public class FileStorageController {
     UploadFileResponseDtoFactory uploadFileResponseDtoFactory;
     StorageService storageService;
-
-    FileUtils fileUtils;
+    UploadProcessor uploadProcessor;
 
     public static final String UPLOAD_FILE = "api/private/file-storage/upload";
 
     @PostMapping(UPLOAD_FILE)
-    public UploadFileResponseDto uploadFile(@RequestParam("file") MultipartFile file) throws FileNotFoundException {
+    public UploadFileResponseDto uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("File info - Name: " + file.getOriginalFilename() +
                 ", Size: " + file.getSize() +
                 ", ContentType: " + file.getContentType());
@@ -39,6 +40,13 @@ public class FileStorageController {
             throw new BadRequestException("File can't be empty");
         }
         FileInfoEntity entity = storageService.temporaryUploadFile(file);
+
+        System.out.println(entity.toString());
+
+        FileInfoEntity compressedEntity = uploadProcessor.compress(entity.getFilePath());
+
+        System.out.println(compressedEntity.toString());
+
         return uploadFileResponseDtoFactory.makeUploadFileResponseDto(entity);
     }
 }
