f7c856c2c7f425034386f3fc955c266528a08ab7
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-02 21:27:55 +0300

    refactor: replace filetypes from FileTypeConfig.java to application.yml
    Добавлен в application.yml список типов файлов:
     - encryption:text-types:...
     - encryption:binary-types:...
     - encryption:compressed-types:...
    
    Типы файлов записываются в FileTypeConfig.java:
     - Set<String> textTypes;

--- file-storage-service/src/main/java/org/example/api/configs/FileTypeConfig.java
+++ file-storage-service/src/main/java/org/example/api/configs/FileTypeConfig.java
@@ -3,6 +3,7 @@ package org.example.api.configs;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +13,12 @@ import java.util.Set;
 
 @Component
 @ConfigurationProperties(prefix = "compression.file.types")
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 public class FileTypeConfig {
-    Set<String> textTypes = new HashSet<>(
-            Arrays.asList("txt", "log", "md", "csv", "tsv", "html", "htm", "xml", "json", "ini", "conf")
-    );
+    @Value("${encryption.text-types}")
+    Set<String> textTypes;
+
+    @Value("${encryption.binary-types}")
+    Set<String> binaryTypes;
 }
