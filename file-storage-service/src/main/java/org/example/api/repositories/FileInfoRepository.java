a5b8eb3f0abb5768f37b77aacdd057ad692e0181
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-12 15:34:32 +0300

    feat: add Repository annotation

--- file-storage-service/src/main/java/org/example/api/repositories/FileInfoRepository.java
+++ file-storage-service/src/main/java/org/example/api/repositories/FileInfoRepository.java
@@ -2,10 +2,12 @@ package org.example.api.repositories;
 
 import org.example.api.entities.FileInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface FileInfoRepository extends JpaRepository<FileInfoEntity, UUID> {
     Optional<FileInfoEntity> findByFilePath(String filePath);
 }
