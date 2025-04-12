50a7c8425c84dc6e0e9fd8abe5573dea65fc578e
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-12 15:39:34 +0300

    feat: add logic in PermanentStorageService.java
    Добавлен метод permanentUploadFile(String path) для сохранения файла в постоянное хранилище

--- file-storage-service/src/main/java/org/example/api/services/storage/PermanentStorageService.java
+++ file-storage-service/src/main/java/org/example/api/services/storage/PermanentStorageService.java
@@ -3,9 +3,13 @@ package org.example.api.services.storage;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.example.api.dto.service.PermanentFileDto;
+import org.example.api.dto.service.StoredFileDto;
+import org.example.api.entities.FolderEntity;
+import org.example.api.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -13,11 +17,22 @@ import org.springframework.stereotype.Service;
 public class PermanentStorageService {
     @Value("${PATH_TO_PERMANENT_STORAGE}")
     private String permanentStoragePath;
+    final FolderService folderService;
+    final FileUtils fileUtils;
 
-    public PermanentFileDto permanentUploadFile() {
-        return PermanentFileDto.builder()
-                .path("")
-                .fileSize((long) 1)
+    public StoredFileDto permanentUploadFile(String path) {
+        FolderEntity folder = folderService.getLeastFilledFolder();
+        System.out.println(folder.getPath());
+        String folderPath = folder.getPath();
+
+        String fileName = fileUtils.getFileName(path);
+        File file = fileUtils.getFileOrThrowException(path);
+        String storedPath = fileUtils.createFileInDir(fileName, file, folderPath);
+
+        return StoredFileDto.builder()
+                .path(storedPath)
+                .fileSize(file.length())
+                .folderEntity(folder)
                 .build();
     }
 }
