9414a34bb7b9a0164c4ad85d1117709ff2fe6c5d
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-12 15:41:36 +0300

    feat: add FolderEntity folderEntity for StoredFileDto.java
    Добавлено поле, хранящее сущность узла хранилище, в который был сохранен файл

--- file-storage-service/src/main/java/org/example/api/dto/service/StoredFileDto.java
+++ file-storage-service/src/main/java/org/example/api/dto/service/StoredFileDto.java
@@ -2,13 +2,15 @@ package org.example.api.dto.service;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.example.api.entities.FolderEntity;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class PermanentFileDto {
+public class StoredFileDto {
     String path;
     Long fileSize;
+    FolderEntity folderEntity;
 }
