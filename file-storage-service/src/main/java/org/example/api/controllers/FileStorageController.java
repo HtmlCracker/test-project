d79fb108d5dede2361df827bf2f26df0aa430456
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-16 20:58:05 +0300

    feat: integrate FileProcessorService logic for uploadFile endpoint
    Внедрил логику обработки файла через StateMachine:
     - Файл сохраняется во временную директорию
     - Сохраняется сущность файла
     - Файл проходит через состояния (UPLOADED, COMPRESSED, ENCRYPTED, STORED)
     - Возвращается UploadFileResponseDto

--- file-storage-service/src/main/java/org/example/api/controllers/FileStorageController.java
+++ file-storage-service/src/main/java/org/example/api/controllers/FileStorageController.java
@@ -7,42 +7,42 @@ import lombok.experimental.FieldDefaults;
 import org.example.api.dto.response.UploadFileResponseDto;
 import org.example.api.entities.FileInfoEntity;
 import org.example.api.exceptions.BadRequestException;
+import org.example.api.exceptions.NotFoundException;
 import org.example.api.factories.response.UploadFileResponseDtoFactory;
+import org.example.api.repositories.FileInfoRepository;
+import org.example.api.services.FileProcessorService;
 import org.example.api.services.storage.TemporaryStorageService;
-import org.example.api.services.UploadProcessor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @RequiredArgsConstructor
 @Transactional
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RestController
 public class FileStorageController {
+    FileInfoRepository fileInfoRepository;
     UploadFileResponseDtoFactory uploadFileResponseDtoFactory;
     TemporaryStorageService storageService;
-    UploadProcessor uploadProcessor;
+    FileProcessorService fileProcessorService;
 
     public static final String UPLOAD_FILE = "api/private/file-storage/upload";
 
     @PostMapping(UPLOAD_FILE)
-    public UploadFileResponseDto uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        System.out.println("File info - Name: " + file.getOriginalFilename() +
-                ", Size: " + file.getSize() +
-                ", ContentType: " + file.getContentType());
-
+    public UploadFileResponseDto uploadFile(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             throw new BadRequestException("File can't be empty");
         }
         FileInfoEntity entity = storageService.temporaryUploadFile(file);
 
-        FileInfoEntity compressedEntity = uploadProcessor.compress(entity.getFilePath());
-        FileInfoEntity encryptedEntity = uploadProcessor.encrypt(compressedEntity.getFilePath());
-        FileInfoEntity storedEntity = uploadProcessor.store(encryptedEntity.getFilePath());
-        System.out.println(storedEntity.getFilePath());
-        return uploadFileResponseDtoFactory.makeUploadFileResponseDto(storedEntity);
+        fileProcessorService.processFile(entity.getId().toString());
+
+        FileInfoEntity fileInfoEntity = fileInfoRepository.findById(entity.getId())
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("Entity with id: %s is not exists", entity.getId().toString()))
+                );
+
+        return uploadFileResponseDtoFactory.makeUploadFileResponseDto(fileInfoEntity);
     }
 }
