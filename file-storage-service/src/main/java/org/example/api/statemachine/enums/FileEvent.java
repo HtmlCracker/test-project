31fcf855e45af1a064970649e5f24ff4c5d7c792
HtmlCracker <daniilprogrammer3.14@gmail.com>
2025-04-16 20:51:25 +0300

    feat: add enums for StateMachine
    Добавлены энумы для машины состояний:
     - FileEvent( COMPRESS, ENCRYPT, STORE, RETRY )
     - FileState( UPLOADED, COMPRESSED, ENCRYPTED, STORED)

---
+++ file-storage-service/src/main/java/org/example/api/statemachine/enums/FileEvent.java
@@ -0,0 +1,8 @@
+package org.example.api.statemachine.enums;
+
+public enum FileEvent {
+    COMPRESS,
+    ENCRYPT,
+    STORE,
+    RETRY
+}
---
+++ file-storage-service/src/main/java/org/example/api/statemachine/enums/FileState.java
@@ -0,0 +1,8 @@
+package org.example.api.statemachine.enums;
+
+public enum FileState {
+    UPLOADED,
+    COMPRESSED,
+    ENCRYPTED,
+    STORED
+}
